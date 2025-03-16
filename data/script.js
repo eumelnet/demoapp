d71c3c234daba99660407855dd70d3d4d1185e29
Frank Kloeker <f.kloeker@telekom.de>
2025-03-16 19:58:30 +0100

    use api key

--- data/script.js
+++ data/script.js
@@ -1,12 +1,31 @@
 import * as XLSX from 'xlsx';
 
 // Replace with the actual URL of your Google Sheet (Published as CSV)
-const googleSheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTQrG3DGoGk8vXN5E_2LhR0-8mN6vOQ6jz1Cq5J2wcVjS0Q/pub?output=xlsx';
-const corsProxy = 'https://corsproxy.io/?';
+
+//const googleSheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTQrG3DGoGk8vXN5E_2LhR0-8mN6vOQ6jz1Cq5J2wcVjS0Q/pub?output=xlsx';
+//const corsProxy = 'https://corsproxy.io/?';
+
+const API_KEY = 'AIzaSyCLn7jAXNPZEdqCh4N3DkDsGeg4-PgTq30';
+const SHEET_ID = '1Dxi1SBylvqnO7RaB5oZ5k9RL20qokh9vIuQOpR15Igk';
+const RANGE = 'Sheet1!A1:D10';
+
+fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.values);
+  })
+  .catch(err => console.error(err));
+
 
 async function fetchData() {
     try {
-        const response = await fetch(corsProxy + googleSheetURL);
+        const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`)
+	  .then(res => res.json())
+	  .then(data => {
+  	  console.log(data.values);
+ 	 })
+	  .catch(err => console.error(err));
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
